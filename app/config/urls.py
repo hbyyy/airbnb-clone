60ab0d823428c4b7c02b0f2933492b9d9bd484c2
hbyyy <qjaduddl94@gmail.com>
2020-02-03 17:48:11 +0900

    feat: home 페이지 작업 1. paginator 를 이용해 page별로 표시할 수 있도록 함

--- app/config/urls.py
+++ app/config/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from config import settings
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-              ] + static(prefix=settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('', include('core.urls', namespace='core')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(prefix=settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/rooms/views.py
+++ app/rooms/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage
+from django.shortcuts import render, redirect
 
+from rooms import models as rooms_models
+
+from math import ceil
 # Create your views here.
+
+
+def all_room(request):
+    # page = request.GET.get('page', 1)
+    # page = int(page or 1)
+    # page_size = 10
+    # limit = page * page_size
+    # offset = limit - page_size
+    # all_rooms = rooms_models.Room.objects.all()[offset: limit]
+    # page_count = ceil(rooms_models.Room.objects.count() / page_size)
+    # context = {
+    #     'rooms': all_rooms,
+    #     'page': page,
+    #     'page_count': page_count,
+    #     'page_range': range(1, page_count +1)
+    #
+    # }
+
+    #use pagenator
+    page = request.GET.get('page', 1)
+    page_size = 10
+    all_rooms = rooms_models.Room.objects.all()
+    paginator = Paginator(all_rooms, page_size, orphans=5)
+
+    try:
+        rooms = paginator.page(int(page))
+        context = {
+            'page': rooms
+        }
+        return render(request, 'rooms/home.html', context)
+    except EmptyPage:
+        return redirect('/')
+
